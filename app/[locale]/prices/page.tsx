a46247bdf19351adc929d2b58c7eed065f3f90b7
Sapper <saniamykhailenko@gmail.com>
2026-01-22 19:29:31 +0200

    add: hero in prices page

--- app/[locale]/prices/page.tsx
+++ app/[locale]/prices/page.tsx
@@ -1,10 +1,16 @@
+import { useTranslations } from "next-intl";
 import css from "./Prices.module.css";
 
 export default function Prices() {
+  const t = useTranslations("pricesHero");
+
   return (
     <section className={css.heroPrice}>
       <div className={"container"}>
-        <h1 className={css.heroTitle}>ЦІНИ на ПОСЛУГИ</h1>
+        <h1 className={css.heroTitle}>
+          <span className={css.textTop}>{t("titleTop")}</span>
+          <span className={css.textBottom}>{t("titleBottom")}</span>
+        </h1>
       </div>
     </section>
   );
